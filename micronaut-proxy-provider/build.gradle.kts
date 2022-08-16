792b440f0dc537824c47b09b6c7014094a557eb9
Richard Agyei <richard.agyei@gmail.com>
2022-08-16 08:06:58 -0500

    Micronaut proxy provider

--- micronaut-proxy-provider/build.gradle.kts
+++ micronaut-proxy-provider/build.gradle.kts
@@ -3,11 +3,36 @@
 //}
 plugins {
     id("io.richard.event.processor.java-library-conventions")
+//    id 'java-library'
+//    id "io.micronaut.library" version "2.0.8"
 }
 
 dependencies {
     implementation("org.slf4j:slf4j-api")
     implementation("org.slf4j:slf4j-simple")
+
+    annotationProcessor "io.micronaut:micronaut-inject-java"
+    implementation "io.micronaut:micronaut-inject-java"
+
+    /*
+    annotationProcessor "io.micronaut:micronaut-inject-java"
+    implementation "io.micronaut:micronaut-inject-java"
+
+    implementation("io.vavr:vavr:${vavrVersion}")
+
+    testAnnotationProcessor "io.micronaut:micronaut-inject-java"
+
+    testImplementation("javax.annotation:javax.annotation-api:1.3.2")
+    testImplementation("io.micronaut.test:micronaut-test-core")
+    testImplementation("io.micronaut.test:micronaut-test-junit5")
+    testImplementation("io.micronaut:micronaut-inject")
+    testImplementation("io.micronaut:micronaut-inject-java")
+
+    testImplementation(platform("org.junit:junit-bom:${junitVersion}"))
+    testImplementation('org.junit.jupiter:junit-jupiter')
+    testImplementation("org.assertj:assertj-core:${assertjVersion}")
+     */
+
 
     implementation(project(":annotations"))
     annotationProcessor(project(":annotation-processor"))
@@ -16,7 +41,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
-}
+//
+//tasks.getByName<Test>("test") {
+//    useJUnitPlatform()
+//}
